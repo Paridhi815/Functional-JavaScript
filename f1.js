90acba6cf1900d2c606cbd5ae28867ca882038e8
paridhi <paridhi815@gmail.com>
2018-01-16 10:21:36 +0530

    added non string and null cases

--- f1.js
+++ f1.js
@@ -1,8 +1,13 @@
 function upperCaser(input)
 {
-  return (input.toUpperCase());
+  if(input!="" && typeof(input)==="string")
+    return (input.toUpperCase());
+  else
+    return false;
 }
 module.exports=upperCaser
-console.log("Print String in upper case:",upperCaser("Pari")=="PARI");
-console.log("Print String which includes numbers in upper case:",upperCaser("567hgh")=="567HGH");
-console.log("Print String which includes special characters in upper case:",upperCaser("nvs&^$#/;;'hgsh'")=="NVS&^$#/;;'HGSH'");
+console.log("Print String in upper case:",upperCaser("Pari")==="PARI");
+console.log("Print String which includes numbers in upper case:",upperCaser("567hgh")==="567HGH");
+console.log("Print String which includes special characters in upper case:",upperCaser("nvs&^$#/;;'hgsh'")==="NVS&^$#/;;'HGSH'");
+console.log("Print String which is a NULL in upper case:",upperCaser("")===false);
+console.log("Print for cases where parameter isnt a string:",upperCaser(12345)===false);
