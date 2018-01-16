13c0d733aa1d28be7cbc3eec4f79dd4062c95786
paridhi <paridhi815@gmail.com>
2018-01-16 10:05:50 +0530

    printed with true

--- f1.js
+++ f1.js
@@ -3,6 +3,6 @@ function upperCaser(input)
   return (input.toUpperCase());
 }
 module.exports=upperCaser
-console.log("Print String in upper case:",upperCaser("Pari"));
-console.log("Print String which includes numbers in upper case:",upperCaser("567hgh"));
-console.log("Print String which includes special characters in upper case:",upperCaser("nvs&^$#/;;'hgsh'"));
+console.log("Print String in upper case:",upperCaser("Pari")=="PARI");
+console.log("Print String which includes numbers in upper case:",upperCaser("567hgh")=="567HGH");
+console.log("Print String which includes special characters in upper case:",upperCaser("nvs&^$#/;;'hgsh'")=="NVS&^$#/;;'HGSH'");
