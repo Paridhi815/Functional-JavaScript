4b7fa7a0df44e10860be8e47c0f5825173e49c50
paridhi <paridhi815@gmail.com>
2018-01-16 14:13:41 +0530

    added all

--- f1.js
+++ f1.js
@@ -7,7 +7,9 @@ function upperCaser(input)
 }
 module.exports=upperCaser
 console.log("Print String in upper case:",upperCaser("Pari")==="PARI");
-console.log("Print String which includes numbers in upper case:",upperCaser("567hgh")==="567HGH");
-console.log("Print String which includes special characters in upper case:",upperCaser("nvs&^$#/;;'hgsh'")==="NVS&^$#/;;'HGSH'");
-console.log("Print String which is a NULL in upper case:",upperCaser("")===false);
-console.log("Print for cases where parameter isnt a string:",upperCaser(12345)===false);
+//no other test case required,one is enough.We assume input is valid.
+// console.log("Print String which includes numbers in upper case:",upperCaser("567hgh")==="567HGH");
+// console.log("Print String which includes special characters in upper case:",upperCaser("nvs&^$#/;;'hgsh'")==="NVS&^$#/;;'HGSH'");
+// console.log("Print String which is a NULL in upper case:",upperCaser("")===false);
+// console.log("Print for cases where parameter isnt a string:",upperCaser(12345)===false);
+// console.log("Print String which is already in upper case:",upperCaser("PARI")==="PARI");
