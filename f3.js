e9059745fcaf1172b1b662c3c39a054567e091c7
paridhi <paridhi815@gmail.com>
2018-01-16 12:39:30 +0530

    added test case for empty arrays

--- f3.js
+++ f3.js
@@ -8,3 +8,4 @@ module.exports = doubleAll
 
 console.log("Should pass basic test case:",doubleAll([1,2,3,4,5]).toString()===[2,4,6,8,10].toString());
 console.log("Should pass for arrays with negative numbers:",doubleAll([1,-2,-3,4,5]).toString()===[2,-4,-6,8,10].toString());
+console.log("Should pass for null arrays:",doubleAll([]).toString()===[].toString());
