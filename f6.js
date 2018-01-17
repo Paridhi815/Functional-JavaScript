8546f83aa02b988d729f85c364ef439544a1d063
paridhi <paridhi815@gmail.com>
2018-01-17 13:01:21 +0530

    added empty test case

--- f6.js
+++ f6.js
@@ -1,16 +1,27 @@
 function countWords (inputWords) {
   return inputWords.reduce(function (count, word) {
-    if (count[word]) {
-      count[word] = ++count[word]
-    } else {
-      count[word] = 1
-    }
+    count[word] = ++count[word] || 1
     return count
   }, {})
 }
 
 module.exports = countWords
+// basic
 let inputWords = ['Apple', 'Banana', 'Apple', 'Durian', 'Durian', 'Durian']
 console.log('Should run the basic case:', countWords(inputWords))
+
+// empty
 inputWords = []
 console.log('Should run the empty array:', countWords(inputWords))
+
+// all same
+inputWords = ['apple', 'apple', 'apple', 'apple', 'apple']
+console.log('Should run the same element array:', countWords(inputWords))
+
+// all different
+inputWords = ['Apple', 'Banana', 'Grapes', 'Durian', 'Chiku', 'Pineapple']
+console.log('Should run the basic case:', countWords(inputWords))
+
+// only one
+inputWords = ['Apple']
+console.log('Should run the case with 1 element:', countWords(inputWords))
